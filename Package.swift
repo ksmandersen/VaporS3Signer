e1b32c3e000a66d6369546f26ae3049a7eff113e
Kristian Andersen <hello@kristian.co>
2017-06-29 15:54:09 +0200

    Pin agains ksmandersen

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "VaporS3Signer",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/JustinM1/S3SignerAWS.git",
+        .Package(url: "https://github.com/ksmandersen/S3SignerAWS.git",
                  majorVersion: 2),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
